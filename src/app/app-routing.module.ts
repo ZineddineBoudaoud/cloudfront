5867d6c94f789e2e08ac9da35f49732d299700c2
Zineddine <zinedddine97@gmail.com>
2020-02-10 23:13:52 +0100

    fix redirection in webapp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AquariumComponent } from './aquarium/aquarium.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aquariums', component: AquariumComponent },
+  { path: '**', component: AquariumComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
